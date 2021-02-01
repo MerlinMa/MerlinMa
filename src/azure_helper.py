c3d3c54bf9d73bb303010bf625f78cb3721581e5
facac3c4-e2a5-4257-af76-205c8a821ddb\\ABBLBZZ@mmm.com <abblbzz@mmm.com>
2021-02-01 16:02:11 -0600

    refactor upload_file and upload_data to turn overwrite on/off Added an optional parameter: overwrite

--- src/azure_helper.py
+++ src/azure_helper.py
@@ -39,11 +39,14 @@ class AzureHelper:
         try:
             container_client.get_container_properties()
         except:
-            # TODO specify an exception type
             self.blob_service_client.create_container(self.config['container_name'])
 
 
-    def upload_data(self, blob_contents, blob_name: str, blob_subdir: str = None):
+    def upload_data(self,
+                    blob_contents,
+                    blob_name: str,
+                    blob_subdir: str = None,
+                    overwrite: bool = True):
         """
         Uploads data to a blob in Azure storage
 
@@ -59,7 +62,8 @@ class AzureHelper:
         blob_subdir : str, default None
             The subdirectory within the container where the blob should be located
             This will be appended at the beginning of blob_name
-
+        overwrite : bool, default True
+            boolian value which decides if the blob will be overwritten if it already exists
         """
         if blob_contents is None:
             raise ValueError("blob_contents cannot be None")
@@ -79,10 +83,14 @@ class AzureHelper:
         blob_client = self.blob_service_client.get_blob_client(
             self.config["container_name"], blob_name
             )
-        blob_client.upload_blob(blob_contents, overwrite=True)
+        blob_client.upload_blob(blob_contents, overwrite=overwrite)
 
 
-    def upload_file(self, local_filename: str, blob_name: str = None, blob_subdir: str = None):
+    def upload_file(self,
+                    local_filename: str,
+                    blob_name: str = None,
+                    blob_subdir: str = None,
+                    overwrite: bool = True):
         """
         Uploads a file to a blob in Azure storage
 
@@ -92,12 +100,12 @@ class AzureHelper:
             The relative filepath of the file to upload
         blob_name : str, default value
             Creates a new blob if a blob does not exist with the given blob_name
-            Overwrites the existing blob if a blob already exists with the given blob_name
             If no blob_name is provided then the name of the local file will be used instead
         blob_subdir : str, default None
             This will be appended at the beginning of blob_name
-
-    """
+        overwrite : bool, default True
+            boolian value which decides if the blob will be overwritten if it already exists
+        """
         if local_filename is None:
             raise ValueError("local_filename cannot be None")
         if blob_name is None:
@@ -107,4 +115,4 @@ class AzureHelper:
 
         filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), local_filename)
         with open(filepath, 'rb') as blob_contents:
-            self.upload_data(blob_contents, blob_name, blob_subdir)
+            self.upload_data(blob_contents, blob_name, blob_subdir, overwrite=overwrite)

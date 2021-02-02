1f02641e8eb70872a138ad89f40a83dc234ad5cc
facac3c4-e2a5-4257-af76-205c8a821ddb\\ABBLBZZ@mmm.com <abblbzz@mmm.com>
2021-02-02 14:26:59 -0600

    Update hello_world_entry documentation to be more consistent with template

--- src/hello_world_entry.py
+++ src/hello_world_entry.py
@@ -2,19 +2,19 @@
 hello_world_entry
 ---------
 This script is intended to be an introduction to PALS excecutions
+Use this script to test that your PALS deployment is working
 
+Results are accessible from the Process Studio REST API
+More information found here: https://we.mmm.com/wiki/pages/viewpage.action?pageId=504015696
 """
 
 def main_entry_point(dict_main_entry_point_args: dict) -> dict:
-    """
-    Main driving method called by PALS executor
-    This is were all data processing would normally occur
-    For this example we return a dictionary containing the message "Hello, world!"
-    Use this script to test that your PALS deployment is working
-
-    dict_main_entry_point_args is the data gathered by PALS
-    Use this function to process, upload, save, etc that data
-    """
+    """Main driving method called by PALS executor"""
+    # dict_main_entry_point_args is the data gathered by PALS
+    # This is were all data processing will normally occur
+    # Use this function to process, analyze, upload, save, etc.
+    # For a showcase of the functionality currently developed see template_entry_script.py
+    # For this example we return a dictionary containing the message "Hello, world!"
 
     dict_results = {'Message': 'Hello, world!'}
 

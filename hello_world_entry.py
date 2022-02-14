1c5cf90bd6f2b787da394d6940a863b570c659ea
Nick Blum <a2fj2zz@mmm.com>
2022-02-14 11:28:17 -0600

    Updated string test script

--- hello_world_entry.py
+++ hello_world_entry.py
@@ -23,7 +23,7 @@ def main_entry_point(dict_main_entry_point_args: dict) -> dict:
         }
 
     ############################ Transform Input Data to DataFrame ##############################
-    df_data = PALSMethods.dictionary_to_dataframe(dict_main_entry_point_args, sStatistic="Average")
+    df_data = PALSMethods.dictionary_to_dataframe(dict_main_entry_point_args)
 
     ############################ Load config ##########################################
     root_dir = os.path.dirname(os.path.abspath(__file__))
@@ -31,13 +31,17 @@ def main_entry_point(dict_main_entry_point_args: dict) -> dict:
     dict_lookup = PALSMethods.load_config(os.path.join(root_dir, 'lookup.json'))
 
     ############################ Get Tag Value ##########################################
-    sTimestamp = dict_main_entry_point_args.get('PeriodicStatistics').get('Timestamps')[0]
-    dict_TagValue = get_tag_value(26, sTimestamp)    
-    sCurrentRecipe = dict_TagValue.get('Value')
+    sStartTimestampUTC = dict_main_entry_point_args.get('PeriodicValues').get('Timestamps')[0]
+    sEndTimestampUTC = dict_main_entry_point_args.get('PeriodicValues').get('Timestamps')[-1]
+
+    #dict_TagValue = get_tag_value(26, sStartTimestampUTC) 
+
+    dict_TagValues = get_tag_values(1, sStartTimestampUTC, sEndTimestampUTC, 60)   
+    lstValues = [item['Value'] for item in dict_TagValues['Items']]
+    df_data['BOOL_TAG'] = lstValues
 
     ############################ Lookup Recipe Value ##########################################
     #dNipGap = dict_lookup.get('GROUPED_MEDIAN_NIP_GAP').get(sCurrentRecipe)    
-    df_data['RECIPE'] = sCurrentRecipe
 
     ############################ Fill Results Dictionary ##########################################
     dict_results.get('Data').extend(PALSMethods.dataframe_to_list(df_data))
@@ -50,9 +54,9 @@ def main_entry_point(dict_main_entry_point_args: dict) -> dict:
 ###########################################
 def get_tag_value(nTagKey: int(), sTimestampUTC: str()):
 
-    sTagValueURL = f'http://dsfpsdemo.mmm.com/processstudio/demo/workcenters/DSFLINE1/tags/{nTagKey}/value?Timestamp={sTimestampUTC}'
-
     dictResponse = dict()
+
+    sTagValueURL = f'http://dsfpsdemo.mmm.com/processstudio/demo/workcenters/DSFLINE1/tags/{nTagKey}/value?Timestamp={sTimestampUTC}'
 
     nStatusCode, dictResponse = AIMRequest.Get(sTagValueURL, bBinaryMode=True)
     if ( nStatusCode < 200 or nStatusCode >= 300 ):
@@ -62,11 +66,28 @@ def get_tag_value(nTagKey: int(), sTimestampUTC: str()):
     return dictResponse
 
 ###########################################
+# get_tag_values
+###########################################
+def get_tag_values(nTagKey, sStartDateTimeUTC, sEndDateTimeUTC, sSamplingPeriod):
+
+    dictResponse = dict()
+    
+    sTagValuesURL = f'http://dsfpsdemo.mmm.com/processstudio/demo/workcenters/DSFLINE1/tags/{nTagKey}/values?StartTimestamp={sStartDateTimeUTC}&EndTimestamp={sEndDateTimeUTC}&SamplingPeriodS={sSamplingPeriod}'
+    
+    nStatusCode, dictResponse = AIMRequest.Get(sTagValuesURL, bBinaryMode=False)
+    if ( nStatusCode < 200 or nStatusCode >= 300 ):
+        AIMMethods._handle_exception ( dictResponse )
+        dictResponse = {}
+
+    return dictResponse
+
+
+###########################################
 # LOCAL TESTING FUNCTION
 ###########################################
 if __name__ == "__main__":
 
-    with open('test_files/test_periodic_statistics.json') as json_file:
+    with open('test_files/test_periodic_values.json') as json_file:
         ENTRY_POINT_ARGS = json.load(json_file)
 
     print(main_entry_point(ENTRY_POINT_ARGS))
